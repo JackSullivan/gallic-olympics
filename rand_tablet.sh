6b9b8f3d31c8ffb29036e786cc972821dcdb8990
John Sullivan <john.t.sullivan@gmail.com>
2014-03-07 16:51:15 -0500

    added script for random tablets.

---
+++ rand_tablet.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp target/gaulish-olympics-1.0-SNAPSHOT-jar-with-dependencies.jar so.modernized.runners.SpawnRandomTablet 127.0.0.1 2552 1000 "Rome|Gaul|Carthage" "Curling|Biathlon|Slalom"
