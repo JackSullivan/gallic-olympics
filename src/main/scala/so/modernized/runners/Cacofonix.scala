581c6076ea27a2bac364829e706e48ef0639b5ff
Ari Kobren <akobren@cs.umass.edu>
2014-03-07 17:09:05 -0500

    randomized cacofonix

--- src/main/scala/so/modernized/runners/Cacofonix.scala
+++ src/main/scala/so/modernized/runners/Cacofonix.scala
@@ -2,6 +2,8 @@ package so.modernized.runners
 
 import so.modernized.CacofonixClient
 import akka.actor.Address
+import scala.util.Random
+import scala.collection.mutable
 
 /**
  * @author John Sullivan
@@ -10,15 +12,23 @@ object Cacofonix {
   def main(args: Array[String]) = {
     val host = args(0)
     val port = args(1).toInt
+    val events = args(2).split('|')
+    val teams = args(3).split('|')
 
     val cacofonix = new CacofonixClient(Address("akka.tcp","olympics", host, port))
+    val eventToScore = new mutable.HashMap[String, mutable.HashMap[String, Int]]
 
-    println("Let the games begin!")
-    cacofonix.setScore("Biathlon", "Gaul 1, Rome 0, Carthage 0")
-    cacofonix.setScore("Curling", "Gaul 1, Rome 0, Carthage 1")
-    Thread.sleep(1000)
-    cacofonix.setScore("Biathlon", "Gaul 2, Rome 0, Carthage 0")
-    cacofonix.setScore("Curling", "Gaul 3, Rome 2, Carthage 1")
-    Thread.sleep(1000)
+    println("Cacofonix is here and ready to report on the games!")
+    (0 until 100).foreach(_ => {
+      val event = events(Random.nextInt(events.length))
+      val team = teams(Random.nextInt(teams.length))
+      val scores = eventToScore.getOrElseUpdate(event, new mutable.HashMap[String, Int])
+
+      val score = scores.getOrElse(team, 0)
+      scores.update(team, score + 1)
+
+      cacofonix.setScore(event, scores.toSeq.map({ case (t,s) => t + " " + s }).mkString(", "))
+      Thread.sleep(1000)
+    })
   }
 }
