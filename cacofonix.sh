238ae1543b5b1fe15d38003a4c07b35abfce8dc5
John Sullivan <john.t.sullivan@gmail.com>
2014-03-07 20:02:07 -0500

    updatd scripts.

--- cacofonix.sh
+++ cacofonix.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -cp target/gaulish-olympics-1.0-SNAPSHOT-jar-with-dependencies.jar so.modernized.runners.Cacofonix elnux4.cs.umass.edu 2552 "Rome|Gaul|Carthage" "Curling|Biathlon|Piathlon"
+java -cp target/gaulish-olympics-1.0-SNAPSHOT-jar-with-dependencies.jar so.modernized.runners.Cacofonix 127.0.0.1 2552 "Rome|Gaul|Carthage" "Curling|Biathlon|Piathlon"
 
--- rand_tablet.sh
+++ rand_tablet.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-java -cp target/gaulish-olympics-1.0-SNAPSHOT-jar-with-dependencies.jar so.modernized.runners.SpawnRandomTablet 127.0.0.1 2552 1000 "Rome|Gaul|Carthage" "Curling|Biathlon|Slalom"
+
+for i in `seq $1`
+do
+
+java -cp target/gaulish-olympics-1.0-SNAPSHOT-jar-with-dependencies.jar so.modernized.runners.SpawnRandomTablet 127.0.0.1 2552 $2 "Rome|Gaul|Carthage" "Curling|Biathlon|Slalom" > tablet-$i-of-$1-at-$2.log &
+
+done
